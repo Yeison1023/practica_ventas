e1395d4b9dc3a4be75c95b50a6e0ec303ed7b60f
favian silva <fabiansil902@gmail.com>
2025-05-22 09:59:19 -0500

    backend_ventas

---
+++ backend/models/ventaModel.js
@@ -0,0 +1,36 @@
+import { db } from '../config/db.js';
+
+export const registrarVenta = (venta, callback) => {
+    const { id_cliente, id_producto, cantidad} = venta;
+    db.query( 'select precio, stock from productos when id = ?', [id_producto], (err, results) => {
+        if (err) return callback(err);
+        if (results.length === 0 ) {
+            return callback(new Error ('Producto no encontrado'));
+        }
+        const { precio, stock} = results(0);
+        if (cantidad > stock) {
+            return callback (new Error('stock insuficiente'));
+        }
+        const total = precio * cantidad; 
+
+        db.query(
+            'insert into ventas (id_cliente, id_producto, cantidad, precio_unitario, total) values (?, ?, ?, ?, ?)',
+            [id_cliente, id_producto, cantidad, precio, total],
+            (err, resultado) => {
+                if (err) return callback(err);
+                // Actualizar el stock de productos 
+                db,query('update productos set stock = stock = ? where id = ? ', [cantidad, id_producto], (err2) => {
+                    if (err2) return callback(err2); 
+                    callback(null, resultado); 
+                });
+            }
+        )
+
+
+s
+
+
+
+
+    })
+}
